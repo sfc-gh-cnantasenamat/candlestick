cb336315ccbf91d5eef6fb1ab7ed54e66bc7db7f
Chanin Nantasenamat <141535473+sfc-gh-cnantasenamat@users.noreply.github.com>
2024-07-18 13:48:22 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,17 @@
-import streamlit as st
+import plotly.graph_objects as go
+import pandas as pd
+from datetime import datetime
 
-st.title('🎈 App Name')
 
-st.write('Hello world!')
+st.title('🕯️ Candlestick chart')
+
+df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/finance-charts-apple.csv')
+df
+
+fig = go.Figure(data=[go.Candlestick(x=df['Date'],
+                open=df['AAPL.Open'],
+                high=df['AAPL.High'],
+                low=df['AAPL.Low'],
+                close=df['AAPL.Close'])])
+
+st.plotly_chart(fig)
